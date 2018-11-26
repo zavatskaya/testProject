7fab39591d4e176393a26bf8d1c0e593738ed3fc
Liliya Zavatskaya <zavatskaya.liliya@icloud.com>
2018-11-26 12:57:54 +0600

    Initial Commit

---
+++ BitcoinTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BitcoinTest
+//
+//  Created by Liliya Zavatskaya on 11/26/18.
+//  Copyright © 2018 Liliya Zavatskaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
